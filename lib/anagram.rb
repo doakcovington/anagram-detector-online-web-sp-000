b77756da50291ab9b214963d1e75379abf447053
Doak Covington <doakcovington@protonmail.com>
2020-02-07 03:18:03 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+#WDGTR
+
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    #result = []
+    #word_array = @word.split("")
+    #split each element in the array into individual characters
+    individual_characters = array.select{|word| word.split("").sort == @word.split("").sort}
+    #result
+  end
+
+end
